4b8ff5c5de677a1405ae8d5f1bad18c731a53bae
premj01 <premjadhav00002@gmail.com>
2026-03-05 00:47:24 +0530

    UI Modifiend and added ShadCN UI Library

--- kiosk/app/main.js
+++ kiosk/app/main.js
@@ -188,9 +188,9 @@ function createWindow() {
     title: "PrintGo : Easy Printing Solution..",
     width: 800,
     height: 600,
-    kiosk: true,       // fullscreen kiosk mode
-    frame: false,      // no window frame
-    alwaysOnTop: true,
+    // kiosk: true,       // fullscreen kiosk mode
+    // frame: false,      // no window frame
+    // alwaysOnTop: true,
     autoHideMenuBar: true,
     webPreferences: {
       nodeIntegration: true,
